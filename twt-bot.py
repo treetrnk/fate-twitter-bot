56128f83de65097b6b50a2ee47536463178a6f4e
Nathan Hare <nhare330@gmail.com>
2018-10-15 05:36:47 -0400

    Remove unused comments

--- twt-bot.py
+++ twt-bot.py
@@ -5,26 +5,15 @@ from credentials import *
 
 twitter = Twython(app_key, app_secret, access_token, access_token_secret)
 
-"""
-for follower in tweepy.Cursor(api.followers).items():
-    if not follower.following:
-        follower.follow()
-        print ("Followed everyone that is following @" + user.name)
-        sleep(1800)
-"""
-
 def retweet_fav(query):
     print("Retweeting and Favoriting: " + query)
-    result = twitter.search(q=query, count=50, since="2018-10-07")
+    result = twitter.search(q=query, result_type="latest", count=50, since="2018-10-07")
     duplicates = 0
     for tweet in result["statuses"]:
         try:
             twitter.create_favorite(id = tweet["id_str"])
-            print('Favorited')
-            #print('Favorited the tweet by @' + tweet.user.screen_name)
             twitter.retweet(id = tweet["id_str"])
-            #print('Retweeted the tweet by @' + tweet.user.screen_name)
-            print('Retweeted')
+            print('Favorited & Retweeted')
             sleep(60)
 
         except TwythonError as e:
@@ -44,7 +33,7 @@ while 1:
     retweet_fav("#FateRPG")
     counter = 30
     while counter > 0:
-        print(str(counter) + " minutes until next scan")
+        print("  " + str(counter) + " minutes until next scan")
         counter -= 5
         sleep(300)
 
